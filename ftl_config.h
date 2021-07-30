d20565ab4a3623424185836e1b9d8210f4138e22
455LAB <15000846008@163.com>
2021-07-30 19:51:14 +0800

    update ftl config h and memory map h

--- ftl_config.h
+++ ftl_config.h
@@ -4,95 +4,53 @@
 
 #include "nsc_driver.h"
 #include "xparameters.h"
-#include "nvme/nvme.h"
+//#include "nvme/nvme.h"
 
 //checks NSC connection, initializes base address
-#ifdef	XPAR_TIGER4NSC_7_BASEADDR
-#define NSC_7_CONNECTED	1
-#define NSC_7_BASEADDR	XPAR_TIGER4NSC_7_BASEADDR
-#else
-#define NSC_7_CONNECTED	0
-#define NSC_7_BASEADDR	0
-#endif
-#ifdef	XPAR_TIGER4NSC_6_BASEADDR
-#define NSC_6_CONNECTED	1
-#define NSC_6_BASEADDR	XPAR_TIGER4NSC_6_BASEADDR
-#else
-#define NSC_6_CONNECTED	0
-#define NSC_6_BASEADDR	0
-#endif
-#ifdef	XPAR_TIGER4NSC_5_BASEADDR
-#define NSC_5_CONNECTED	1
-#define NSC_5_BASEADDR	XPAR_TIGER4NSC_5_BASEADDR
-#else
-#define NSC_5_CONNECTED	0
-#define NSC_5_BASEADDR	0
-#endif
-#ifdef	XPAR_TIGER4NSC_4_BASEADDR
-#define NSC_4_CONNECTED	1
-#define NSC_4_BASEADDR	XPAR_TIGER4NSC_4_BASEADDR
-#else
-#define NSC_4_CONNECTED	0
-#define NSC_4_BASEADDR	0
-#endif
-#ifdef	XPAR_TIGER4NSC_3_BASEADDR
-#define NSC_3_CONNECTED	1
-#define NSC_3_BASEADDR	XPAR_TIGER4NSC_3_BASEADDR
-#else
-#define NSC_3_CONNECTED	0
-#define NSC_3_BASEADDR	0
-#endif
-#ifdef	XPAR_TIGER4NSC_2_BASEADDR
-#define NSC_2_CONNECTED	1
-#define NSC_2_BASEADDR	XPAR_TIGER4NSC_2_BASEADDR
-#else
-#define NSC_2_CONNECTED	0
-#define NSC_2_BASEADDR	0
-#endif
-#ifdef	XPAR_TIGER4NSC_1_BASEADDR
+#ifdef	XPAR_NANDFLASHCONTROLLER_1_BASEADDR
 #define NSC_1_CONNECTED	1
-#define NSC_1_BASEADDR	XPAR_TIGER4NSC_1_BASEADDR
+#define NSC_1_BASEADDR	XPAR_NANDFLASHCONTROLLER_1_BASEADDR
 #else
 #define NSC_1_CONNECTED	0
 #define NSC_1_BASEADDR	0
 #endif
-#ifdef	XPAR_TIGER4NSC_0_BASEADDR
+#ifdef	XPAR_NANDFLASHCONTROLLER_0_BASEADDR
 #define NSC_0_CONNECTED	1
-#define NSC_0_BASEADDR	XPAR_TIGER4NSC_0_BASEADDR
+#define NSC_0_BASEADDR	XPAR_NANDFLASHCONTROLLER_0_BASEADDR
 #else
 #define NSC_0_CONNECTED	0
 #define NSC_0_BASEADDR	0
 #endif
 
 //number of connected (=AXI mapped) NSC
-#define NUMBER_OF_CONNECTED_CHANNEL (NSC_7_CONNECTED + NSC_6_CONNECTED + NSC_5_CONNECTED + NSC_4_CONNECTED + NSC_3_CONNECTED + NSC_2_CONNECTED + NSC_1_CONNECTED + NSC_0_CONNECTED)
+#define NUMBER_OF_CONNECTED_CHANNEL (NSC_1_CONNECTED + NSC_0_CONNECTED)
 
 
 //--------------------------------
 //NAND flash memory specifications
 //--------------------------------
 
-#define	BYTES_PER_DATA_REGION_OF_NAND_ROW		16384
-#define	BYTES_PER_SPARE_REGION_OF_NAND_ROW		1664
+#define	BYTES_PER_DATA_REGION_OF_NAND_ROW		8192
+#define	BYTES_PER_SPARE_REGION_OF_NAND_ROW		448
 #define BYTES_PER_NAND_ROW						(BYTES_PER_DATA_REGION_OF_NAND_ROW + BYTES_PER_SPARE_REGION_OF_NAND_ROW)
 
 #define	ROWS_PER_SLC_BLOCK			128
-#define	ROWS_PER_MLC_BLOCK			256
+//#define	ROWS_PER_MLC_BLOCK			256
 
 #define	MAIN_BLOCKS_PER_LUN			4096
-#define EXTENDED_BLOCKS_PER_LUN		88
+#define EXTENDED_BLOCKS_PER_LUN		0
 #define TOTAL_BLOCKS_PER_LUN		(MAIN_BLOCKS_PER_LUN + EXTENDED_BLOCKS_PER_LUN)
 
 #define	MAIN_ROWS_PER_SLC_LUN		(ROWS_PER_SLC_BLOCK * MAIN_BLOCKS_PER_LUN)
-#define	MAIN_ROWS_PER_MLC_LUN		(ROWS_PER_MLC_BLOCK * MAIN_BLOCKS_PER_LUN)
+//#define	MAIN_ROWS_PER_MLC_LUN		(ROWS_PER_MLC_BLOCK * MAIN_BLOCKS_PER_LUN)
 
-#define	LUNS_PER_DIE				2
+#define	LUNS_PER_DIE				1
 
 #define	MAIN_BLOCKS_PER_DIE			(MAIN_BLOCKS_PER_LUN * LUNS_PER_DIE)
 #define TOTAL_BLOCKS_PER_DIE		(TOTAL_BLOCKS_PER_LUN * LUNS_PER_DIE)
 
 #define BAD_BLOCK_MARK_PAGE0		0										//first row of a block
-#define BAD_BLOCK_MARK_PAGE1		(ROWS_PER_MLC_BLOCK - 1)				//last row of a block
+#define BAD_BLOCK_MARK_PAGE1		(ROWS_PER_SLC_BLOCK - 1)				//last row of a block
 #define BAD_BLOCK_MARK_BYTE0 		0										//first byte of data region of the row
 #define BAD_BLOCK_MARK_BYTE1 		(BYTES_PER_DATA_REGION_OF_NAND_ROW)		//first byte of spare region of the row
 
@@ -105,27 +63,27 @@
 
 //supported maximum channel/way structure
 #define	NSC_MAX_CHANNELS				(NUMBER_OF_CONNECTED_CHANNEL)
-#define	NSC_MAX_WAYS					8
+#define	NSC_MAX_WAYS					1
 
 //row -> page
-#define	BYTES_PER_DATA_REGION_OF_PAGE			16384
-#define BYTES_PER_SPARE_REGION_OF_PAGE			256		//last 8 byte used by ECC engine (CRC function)
+#define	BYTES_PER_DATA_REGION_OF_PAGE			8192
+#define BYTES_PER_SPARE_REGION_OF_PAGE			448		//last 8 byte used by ECC engine (CRC function)
 // (BYTES_PER_SPARE_REGION_OF_NAND_ROW - BYTES_PER_SPARE_REGION_OF_PAGE) bytes are used by ECC engine (Parity data)
 #define	PAGES_PER_SLC_BLOCK			(ROWS_PER_SLC_BLOCK)
-#define	PAGES_PER_MLC_BLOCK			(ROWS_PER_MLC_BLOCK)
+//#define	PAGES_PER_MLC_BLOCK			(ROWS_PER_MLC_BLOCK)
 
 //ECC encoder/decoder specification
-#define ECC_CHUNKS_PER_PAGE				32
-#define BIT_ERROR_THRESHOLD_PER_CHUNK	20
-#define ERROR_INFO_WORD_COUNT 			11
+//#define ECC_CHUNKS_PER_PAGE				32
+//#define BIT_ERROR_THRESHOLD_PER_CHUNK	20
+//#define ERROR_INFO_WORD_COUNT 			11
 
 
 //------------------------------
 //NVMe Controller Specifications
 //------------------------------
 
-#define	BYTES_PER_NVME_BLOCK		4096
-#define NVME_BLOCKS_PER_PAGE		(BYTES_PER_DATA_REGION_OF_PAGE / BYTES_PER_NVME_BLOCK)
+//#define	BYTES_PER_NVME_BLOCK		4096
+//#define NVME_BLOCKS_PER_PAGE		(BYTES_PER_DATA_REGION_OF_PAGE / BYTES_PER_NVME_BLOCK)
 
 
 
@@ -134,20 +92,20 @@
 //------------------
 
 #define	SLC_MODE				1
-#define	MLC_MODE				2
+//#define	MLC_MODE				2
 
 //************************************************************************
 #define	BITS_PER_FLASH_CELL		SLC_MODE	//user configurable factor
 #define	USER_BLOCKS_PER_LUN		4096		//user configurable factor
 #define	USER_CHANNELS			(NUMBER_OF_CONNECTED_CHANNEL)		//user configurable factor
-#define	USER_WAYS				8			//user configurable factor
+#define	USER_WAYS				1			//user configurable factor
 //************************************************************************
 
-#define	BYTES_PER_DATA_REGION_OF_SLICE		16384		//slice is a mapping unit of FTL
-#define	BYTES_PER_SPARE_REGION_OF_SLICE		256
+#define	BYTES_PER_DATA_REGION_OF_SLICE		8192		//slice is a mapping unit of FTL
+#define	BYTES_PER_SPARE_REGION_OF_SLICE		448
 
 #define SLICES_PER_PAGE				(BYTES_PER_DATA_REGION_OF_PAGE / BYTES_PER_DATA_REGION_OF_SLICE)	//a slice directs a page, full page mapping
-#define NVME_BLOCKS_PER_SLICE		(BYTES_PER_DATA_REGION_OF_SLICE / BYTES_PER_NVME_BLOCK)
+//#define NVME_BLOCKS_PER_SLICE		(BYTES_PER_DATA_REGION_OF_SLICE / BYTES_PER_NVME_BLOCK)
 
 #define	USER_DIES					(USER_CHANNELS * USER_WAYS)
 
--- memory_map.h
+++ memory_map.h
@@ -1,49 +1,3 @@
-//////////////////////////////////////////////////////////////////////////////////
-// memory_map.h for Cosmos+ OpenSSD
-// Copyright (c) 2017 Hanyang University ENC Lab.
-// Contributed by Yong Ho Song <yhsong@enc.hanyang.ac.kr>
-//				  Jaewook Kwak <jwkwak@enc.hanyang.ac.kr>
-//				  Sangjin Lee <sjlee@enc.hanyang.ac.kr>
-//
-// This file is part of Cosmos+ OpenSSD.
-//
-// Cosmos+ OpenSSD is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 3, or (at your option)
-// any later version.
-//
-// Cosmos+ OpenSSD is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-// See the GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Cosmos+ OpenSSD; see the file COPYING.
-// If not, see <http://www.gnu.org/licenses/>.
-//////////////////////////////////////////////////////////////////////////////////
-
-//////////////////////////////////////////////////////////////////////////////////
-// Company: ENC Lab. <http://enc.hanyang.ac.kr>
-// Engineer: Jaewook Kwak <jwkwak@enc.hanyang.ac.kr>
-//
-// Project Name: Cosmos+ OpenSSD
-// Design Name: Cosmos+ Firmware
-// Module Name: Static Memory Allocator
-// File Name: memory_map.h
-//
-// Version: v1.0.0
-//
-// Description:
-//	 - allocate DRAM address space (0x0010_0000 ~ 0x3FFF_FFFF) to each module
-//////////////////////////////////////////////////////////////////////////////////
-
-//////////////////////////////////////////////////////////////////////////////////
-// Revision History:
-//
-// * v1.0.0
-//   - First draft
-//////////////////////////////////////////////////////////////////////////////////
-
 #ifndef MEMORY_MAP_H_
 #define MEMORY_MAP_H_
 
@@ -54,33 +8,33 @@
 #include "request_transform.h"
 #include "garbage_collection.h"
 
-#define DRAM_START_ADDR					0x00100000
+#define DRAM_START_ADDR					XPAR_MIG_0_BASEADDR //xparameters
 
 #define MEMORY_SEGMENTS_START_ADDR		DRAM_START_ADDR
-#define MEMORY_SEGMENTS_END_ADDR		0x001FFFFF
+#define MEMORY_SEGMENTS_END_ADDR		DRAM_START_ADDR + 0x000FFFFF
 
-#define NVME_MANAGEMENT_START_ADDR		0x00200000
-#define NVME_MANAGEMENT_END_ADDR		0x002FFFFF
+#define NVME_MANAGEMENT_START_ADDR		MEMORY_SEGMENTS_END_ADDR + 0x00000001
+#define NVME_MANAGEMENT_END_ADDR		NVME_MANAGEMENT_START_ADDR+ 0x000FFFFF
 
-#define RESERVED0_START_ADDR			0x00300000
-#define RESERVED0_END_ADDR				0x0FFFFFFF
-
-#define FTL_MANAGEMENT_START_ADDR		0x10000000
+#define RESERVED0_START_ADDR			NVME_MANAGEMENT_END_ADDR + 0x00000001
+#define RESERVED0_END_ADDR				RESERVED0_START_ADDR + 0x0FCFFFFF
+ 
+#define FTL_MANAGEMENT_START_ADDR		RESERVED0_END_ADDR + 0x00000001
 // Uncached & Unbuffered
 //for data buffer
-#define DATA_BUFFER_BASE_ADDR 					0x10000000
+#define DATA_BUFFER_BASE_ADDR 					FTL_MANAGEMENT_START_ADDR
 #define TEMPORARY_DATA_BUFFER_BASE_ADDR			(DATA_BUFFER_BASE_ADDR + AVAILABLE_DATA_BUFFER_ENTRY_COUNT * BYTES_PER_DATA_REGION_OF_SLICE)
 #define SPARE_DATA_BUFFER_BASE_ADDR				(TEMPORARY_DATA_BUFFER_BASE_ADDR + AVAILABLE_TEMPORARY_DATA_BUFFER_ENTRY_COUNT * BYTES_PER_DATA_REGION_OF_SLICE)
 #define TEMPORARY_SPARE_DATA_BUFFER_BASE_ADDR	(SPARE_DATA_BUFFER_BASE_ADDR + AVAILABLE_DATA_BUFFER_ENTRY_COUNT * BYTES_PER_SPARE_REGION_OF_SLICE)
 #define RESERVED_DATA_BUFFER_BASE_ADDR 			(TEMPORARY_SPARE_DATA_BUFFER_BASE_ADDR + AVAILABLE_TEMPORARY_DATA_BUFFER_ENTRY_COUNT * BYTES_PER_SPARE_REGION_OF_SLICE)
 //for nand request completion
-#define COMPLETE_FLAG_TABLE_ADDR			0x17000000
+#define COMPLETE_FLAG_TABLE_ADDR			FTL_MANAGEMENT_START_ADDR + 0x07000000
 #define STATUS_REPORT_TABLE_ADDR			(COMPLETE_FLAG_TABLE_ADDR + sizeof(COMPLETE_FLAG_TABLE))
 #define ERROR_INFO_TABLE_ADDR				(STATUS_REPORT_TABLE_ADDR + sizeof(STATUS_REPORT_TABLE))
 #define TEMPORARY_PAY_LOAD_ADDR				(ERROR_INFO_TABLE_ADDR+ sizeof(ERROR_INFO_TABLE))
 // cached & buffered
 // for buffers
-#define DATA_BUFFER_MAP_ADDR		 		0x18000000
+#define DATA_BUFFER_MAP_ADDR		 		FTL_MANAGEMENT_START_ADDR + 0x08000000
 #define DATA_BUFFFER_HASH_TABLE_ADDR		(DATA_BUFFER_MAP_ADDR + sizeof(DATA_BUF_MAP))
 #define TEMPORARY_DATA_BUFFER_MAP_ADDR 		(DATA_BUFFFER_HASH_TABLE_ADDR + sizeof(DATA_BUF_HASH_TABLE))
 // for map tables
@@ -104,8 +58,8 @@
 #define FTL_MANAGEMENT_END_ADDR				((WAY_PRIORITY_TABLE_ADDR + sizeof(WAY_PRIORITY_TABLE))- 1)
 
 #define RESERVED1_START_ADDR				(FTL_MANAGEMENT_END_ADDR + 1)
-#define RESERVED1_END_ADDR					0x3FFFFFFF
 
-#define DRAM_END_ADDR						0x3FFFFFFF
+#define RESERVED1_END_ADDR					XPAR_MIG_0_HIGHADDR
+#define DRAM_END_ADDR						XPAR_MIG_0_HIGHADDR
 
 #endif /* MEMORY_MAP_H_ */
